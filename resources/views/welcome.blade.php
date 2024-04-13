3007cc010c788a152ab0451738e5f44f06ea060c
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-13 16:18:38 +0200

    add file comics.php in config add in web.php const comics add in welcome first part of section main add style of main in app.css
    stamp in page all comics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+<section id="section_main">
+    <div class="jumbotron">
+
+    </div>
+    <div class="container">
+        <div class="title badge-primary">
+            Current Series
+        </div>
+        <div class="comics">
+            <div class="row">
+                @foreach ($comics as $comic )
+                    <div class="col-2">
+                        <div class="card">
+                            <div class="logo">
+                                <img src="{{$comic['thumb']}}" alt="" srcset="">
+                            </div>
+                            <div class="name_comics">
+                                {{$comic['series']}}
+                            </div>
+                        </div> 
+                    </div>
+                @endforeach
+                
+            </div>
+        </div>
+    </div>
+    <div class="load-more ">
+        <button class="badge-primary">load more</button>
+    </div>
+</section>
+    
+
     
 @endsection
