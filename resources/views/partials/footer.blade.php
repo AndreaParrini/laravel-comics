438d6d687bc6aa454d3c0ded526970dd22ba53cf
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-15 19:59:37 +0200

    add section link and section cookies settings add in app.css all style of this section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,69 @@
 <footer>
-    footer
+    <section class="section_links">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div>
+                        <ul>
+                            <h4>Dc comics</h4>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <ul>
+                            <h4>Shop</h4>
+                            <li>Shop DC</li>
+                            <li>Shop Dc Collectibles</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-2">
+                     <div>
+                        <ul>
+                            <h4>DC</h4>
+                            <li>Term Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                        
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>
+                        <ul>
+                            <h4>Sites</h4>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+            <section class="cookies-settings">
+                <div class="row">
+                    <div class="col-">
+                        All Site Content TM and 2020 DC Entertainment, unless otherwise <a href="">noted here</a>.All rights reserved.
+                        <div>
+                            <a href="">Cookies Settings</a>
+                        </div> 
+                    </div> 
+                </div>   
+            </section>
+        </div>
+    </section>
 </footer>
