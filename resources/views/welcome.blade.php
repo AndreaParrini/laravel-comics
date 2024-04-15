dbfe382d452eef42f73386ff21813ea31fbd2621
AndreaParrini <andrea.parrini26@gmail.com>
2024-04-15 19:13:34 +0200

    add section digital comics add style in app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <section id="section_main">
-    <div class="jumbotron">
+    <section class="jumbotron">
 
-    </div>
+    </section>
     <div class="container">
         <div class="title badge-primary">
             Current Series
@@ -27,9 +27,55 @@
             </div>
         </div>
     </div>
-    <div class="load-more ">
+    <section class="load-more ">
         <button class="badge-primary">load more</button>
-    </div>
+    </section>
+    <section class="section-digitalcomics">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="icon">
+                        <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="Digital Comics">
+                        <div>
+                            Digital Comics
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+                        <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="DC Merchandise">
+                        <div>
+                            DC Merchandise
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+                        <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="Subscription">
+                        <div>
+                            Subscription
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+                        <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="Shop Locator">
+                        <div>
+                            Comic Shop Locator
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+                        <img src="{{Vite::asset('resources/images/buy-dc-power-visa.svg')}}" alt="DC Power Visa">
+                        <div>
+                            DC Power Visa
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </section>
     
 
